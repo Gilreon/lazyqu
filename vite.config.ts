b0bca529d7211c3cb2a1d2a2a44713a662b02820
Gilreon Lee <58603241+Gilreon@users.noreply.github.com>
2025-06-02 17:14:31 -0500

    fix: update path alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/', // or '/<repo-name>/' if not using a custom domain
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
